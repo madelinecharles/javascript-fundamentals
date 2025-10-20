b78ea0cf99af3b8b9e7852a5dd8058ed6252bc4c
Madeline Charles <mc0622@byu.edu>
2025-10-20 11:51:45 -0600

    Lessons 1-4

---
+++ index.js
@@ -0,0 +1,34 @@
+"use strict";
+//I'm practicing making comments here
+
+/* this is a multi-
+line comment*/
+
+alert("hello, Madeline!");
+["one", "two"].forEach(alert);
+
+
+let hello = "cat"
+let message;
+message = hello;
+
+alert(message);
+
+//Working with Variables exercises:
+
+let admin, userName;
+userName = "Madeline";
+admin = userName;
+alert ( admin );
+
+let planetName = "Earth";
+let currentUserName = "Madeline";
+
+//Embedded string
+alert(`hello, ${userName}!`);
+alert(`the result is ${1+2}`);
+
+//Boolean
+let isGreater = 4 > 1;
+alert( isGreater );
+

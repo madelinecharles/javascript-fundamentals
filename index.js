d7e926ef13b74662f31f7150301ceb1ed6a9ea9d
Madeline Charles <mc0622@byu.edu>
2025-10-21 16:47:23 -0600

    10/21/2025

--- index.js
+++ index.js
@@ -1,34 +1,74 @@
 "use strict";
+
 //I'm practicing making comments here
 
 /* this is a multi-
 line comment*/
 
-alert("hello, Madeline!");
+console.log("hello, Madeline!");
 ["one", "two"].forEach(alert);
 
 
-let hello = "cat"
+let hello = "Bonjour"
 let message;
 message = hello;
 
-alert(message);
+console.log(message);
 
 //Working with Variables exercises:
 
 let admin, userName;
 userName = "Madeline";
 admin = userName;
-alert ( admin );
+console.log( admin );
 
 let planetName = "Earth";
 let currentUserName = "Madeline";
 
 //Embedded string
-alert(`hello, ${userName}!`);
-alert(`the result is ${1+2}`);
+console.log(`hello, ${userName}!`);
+console.log(`the result is ${1+2}`);
 
 //Boolean
 let isGreater = 4 > 1;
-alert( isGreater );
+console.log( isGreater );
+
+//typeof 
+console.log (typeof admin);
+
+//prompt
+let favoriteColor = prompt('What is your favorite color?', "green");
+alert(`You like the color ${favoriteColor}!`);
+
+//confirm
+let isFrog = confirm("Are you by chance a Frog?");
+alert (isFrog);
+
+//Type Conversions
+
+//convert boolean to string
+let value = false;
+console.log (typeof value); //boolean
+
+value = String(value);
+console.log (typeof value); //now it says string
+
+//Math!
+
+//unary + converts strings into numbers
+console.log(+true); //1
+console.log(+""); //0
+
+//binary + merges strings
+let apples = "2";
+let oranges = "3";
+console.log ( apples + oranges); //23
+console.log ( +apples + +oranges); //5
+
+
+
+
+
+
+
 
